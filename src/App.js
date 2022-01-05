4f3cde70d8b4c7c9d95497015b39c69e0f0658d8
bekhzodx64 <mr.bekzod2018@gmail.com>
2022-01-05 15:08:46 +0500

    Props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
+	const expenses = [
+		{
+			id: 1,
+			title: 'Toilet Paper',
+			amount: 1.5,
+			date: new Date(2021, 6, 2),
+		},
+		{
+			id: 2,
+			title: 'Car Insurance',
+			amount: 7,
+			date: new Date(2021, 6, 6),
+		},
+		{
+			id: 3,
+			title: 'Smart TV',
+			amount: 1000,
+			date: new Date(2021, 6, 8),
+		},
+		{
+			id: 4,
+			title: 'Cougar',
+			amount: 30,
+			date: new Date(2021, 6, 10),
+		},
+	];
+
 	return (
 		<div className='App'>
-			<header>test</header>
+			{expenses.map((expense) => (
+				<ExpenseItem
+					title={expense.title}
+					amount={expense.amount}
+					date={expense.date}
+					key={expense.id}
+				/>
+			))}
 		</div>
 	);
 }
